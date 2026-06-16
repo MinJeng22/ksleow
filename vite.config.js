070ce9a0706c41c49697833ef90ab11787854c92
Chia Min Jeng <ChiaMinJeng@outlook.com>
2026-06-16 17:00:39 +0800

    chore: lock down entry-server output path for vite 6

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,16 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   assetsInclude: ["**/*.png", "**/*.jpg", "**/*.jpeg", "**/*.svg", "**/*.gif"],
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: (chunkInfo) => {
+          if (chunkInfo.name === 'entry-server') {
+            return 'entry-server.js';
+          }
+          return 'assets/[name]-[hash].js';
+        }
+      }
+    }
+  }
 });
